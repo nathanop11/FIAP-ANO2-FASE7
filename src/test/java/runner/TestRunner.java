b50effb01ce77e36ba5cd19ac3c7f60e37737c9e
nathanop11 <nathanop_123@hotmail.com>
2024-10-31 20:41:33 -0300

    add workflow for tests

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,15 +1,13 @@
 package runner;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
-import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("Usuario.feature")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "steps"
+)
 public class TestRunner {
 }
